10878bce983dc169c5460924b82ba4c95b810e59
Elias Gabriel <81771005+elias-gabriel@users.noreply.github.com>
2023-01-10 01:46:02 -0300

    Update dynamicallyGetSchema.py

--- Cloud/Azure/Databricks/PySpark/dynamicallyGetSchema.py
+++ Cloud/Azure/Databricks/PySpark/dynamicallyGetSchema.py
@@ -7,12 +7,14 @@ def get_schema(df):
     for i in range(len(list_schema)):
         if(list_schema[i][1] == 'int'):
             list_schema[i] = (list_schema[i][0], 'integer')
+        elif (list_schema[i][1] == 'bigint'):
+            list_schema[i] = (list_schema[i][0], 'integer')
 
     for i in list_schema:
         fields.append({'metadata':{}, 'name':i[0], 'nullable':True, 'type':i[1]})
 
     final_schema = StructType.fromJson({'fields':fields, 'type':i[1]})
-    return final_schema
+    return final_sche
      
 # using
 df.read.option('schema', final_schema).option('inferschema', True).csv('path')
