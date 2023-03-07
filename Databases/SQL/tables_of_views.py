039106edc4a6158979b124594d2fe3fe94fdbb77
Elias Gabriel <81771005+elias-gabriel@users.noreply.github.com>
2023-03-07 12:08:42 -0300

    Update tables_of_views.py

--- Databases/SQL/tables_of_views.py
+++ Databases/SQL/tables_of_views.py
@@ -11,8 +11,8 @@ cursor = connection.cursor()
 
 sql_agg = """   
 
-SELECT s.name AS SCHEMA_NAME, 
-       v.name AS VIEW_NAME, 
+SELECT v_schema.name AS ViewSchema, v.name AS ViewName, 
+       t_schema.name AS TableSchema, 
        STUFF((SELECT DISTINCT ', ' + t.name 
               FROM sys.views v2 
               JOIN sys.objects o ON v2.object_id = o.object_id 
@@ -20,25 +20,35 @@ SELECT s.name AS SCHEMA_NAME,
               JOIN sys.sql_expression_dependencies ed ON ed.referencing_id = v2.object_id 
               JOIN sys.objects t ON ed.referenced_id = t.object_id 
               WHERE t.type = 'U' AND ed.referenced_class = 1 AND v2.name = v.name 
-              FOR XML PATH('')), 1, 2, '') AS TABLES_IN_VIEW
+              FOR XML PATH('')), 1, 2, '') AS TablesInView
 FROM sys.views v
 JOIN sys.objects o ON v.object_id = o.object_id
-JOIN sys.schemas s ON o.schema_id = s.schema_id
+JOIN sys.schemas v_schema ON o.schema_id = v_schema.schema_id
+JOIN sys.schemas t_schema ON t_schema.schema_id IN (
+    SELECT DISTINCT t.schema_id 
+    FROM sys.views v2 
+    JOIN sys.objects o2 ON v2.object_id = o2.object_id 
+    JOIN sys.schemas s ON o2.schema_id = s.schema_id 
+    JOIN sys.sql_expression_dependencies ed ON ed.referencing_id = v2.object_id 
+    JOIN sys.objects t ON ed.referenced_id = t.object_id 
+    WHERE t.type = 'U' AND ed.referenced_class = 1 AND v2.name = v.name
+)
 WHERE v.is_ms_shipped = 0
-ORDER BY s.name, v.name;
+ORDER BY v_schema.name, v.name, t_schema.name;
 
 """
 
 sql_2 = """ 
 
-SELECT DISTINCT s.name AS SchemaName, v.name AS ViewName, t.name AS TableName
+SELECT DISTINCT v_schema.name AS ViewSchema, v.name AS ViewName, t_schema.name AS TableSchema, t.name AS TableName
 FROM sys.views AS v
 INNER JOIN sys.sql_dependencies AS d ON v.object_id = d.object_id
-INNER JOIN sys.tables AS t ON d.referenced_major_id = t.object_id
-INNER JOIN sys.objects AS o ON t.object_id = o.object_id
-INNER JOIN sys.schemas AS s ON o.schema_id = s.schema_id
+INNER JOIN sys.objects AS t ON d.referenced_major_id = t.object_id
+INNER JOIN sys.objects AS v_obj ON v.object_id = v_obj.object_id
+INNER JOIN sys.schemas AS v_schema ON v_obj.schema_id = v_schema.schema_id
+INNER JOIN sys.schemas AS t_schema ON t.schema_id = t_schema.schema_id
 WHERE v.type = 'V'
-ORDER BY s.name, v.name, t.name;
+ORDER BY v_schema.name, v.name, t_schema.name, t.name;
 
 """
 
