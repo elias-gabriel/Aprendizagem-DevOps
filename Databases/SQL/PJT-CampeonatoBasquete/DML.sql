00ad2a92e2fd13b4698ae8c1b912cefc962296f1
Elias Gabriel <81771005+elias-gabriel@users.noreply.github.com>
2023-05-17 04:08:49 -0300

    Update DML.sql

--- Databases/SQL/PJT-CampeonatoBasquete/DML.sql
+++ Databases/SQL/PJT-CampeonatoBasquete/DML.sql
@@ -17,11 +17,11 @@ INSERT INTO nba.jogadores VALUES
 	('Guilherme', 'Rocha', 'Gui', null, 'B'),
 	('Guilherme', null, null, null, 'B'),
 	('Lucas', null, null, null, 'B')
-  
-  
-  
-  
--- Inserir um jogador: nome, sobrenomme, apelido, datanascimentoYYYY-MM-DD, serie
+	
+
+
+
+-- Inserir um jogador: nome, sobrenomme, apelido, datanascimento, serie
 EXEC nba.InserirJogador 'Nome', 'Sobrenome', 'Apelido', '1990-01-01', 'A';
 
 -- Inserir um local: nome, cidade
@@ -30,15 +30,15 @@ EXEC nba.InserirLocal 'Academia Leao de Juda', 'Aguas Lindas';
 -- Inserir uma partida: data, localID, Jogador1, Vitorias1, Derrotas1, Jogador2, Vitorias2, Derrotas2
 EXEC nba.InserirPartida '2023-05-15', 1, 9, 3, 2, 10, 2, 3;
 EXEC nba.InserirPartida '2023-05-15', 1, 7, 3, 2, 11, 2, 3;
-
 EXEC nba.InserirPartida '2023-05-16', 1, 8, 3, 2, 12, 2, 3;
+
 select * from nba.partidas
--- Inserir estatísticas do jogo: partidaID, JogadorID, Cestasde2, Cestasde3, Faltas, CestasLivres
-EXEC nba.InserirEstatisticasJogo 1, 9, 16, 12, 12, 2;
-EXEC nba.InserirEstatisticasJogo 1, 10, 5, 19, 17, 0;
+-- Inserir estatísticas do jogo: partidaID, JogadorID, Cestasde2, Cestasde3, CestasLivres, Faltas
+EXEC nba.InserirEstatisticasJogo 1, 9, 16, 12, 2, 12;
+EXEC nba.InserirEstatisticasJogo 1, 10, 5, 19, 0, 17;
 
-EXEC nba.InserirEstatisticasJogo 2, 7, 16, 5, 20, 0;
-EXEC nba.InserirEstatisticasJogo 2, 11, 18, 14, 4, 1;
+EXEC nba.InserirEstatisticasJogo 2, 7, 5, 20, 0, 11;
+EXEC nba.InserirEstatisticasJogo 2, 11, 18, 14, 1, 4;
 
-EXEC nba.InserirEstatisticasJogo 3, 8, 46, 99, 7, NULL;
-EXEC nba.InserirEstatisticasJogo 3, 12, 14, 34, 2, null;
+EXEC nba.InserirEstatisticasJogo 3, 8, 23, 33, 0, 7;
+EXEC nba.InserirEstatisticasJogo 3, 12, 14, 34, 0, 2;
