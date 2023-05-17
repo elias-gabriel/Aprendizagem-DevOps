57eaabff6503f9e574571bbe28238468ad0f98f3
Elias Gabriel <81771005+elias-gabriel@users.noreply.github.com>
2023-05-17 03:42:45 -0300

    Create DML.sql

---
+++ Databases/SQL/PJT-CampeonatoBasquete/DML.sql
@@ -0,0 +1,44 @@
+-- Serie A
+
+INSERT INTO nba.jogadores VALUES 
+	('Fernando', 'Rodrigues', 'Nando', NULL, 'A'),
+	('Bruno', 'de Lucas', 'Dex', '2006-08-03', 'A'),
+	('Elias', 'Gabriel', 'Big Fellas', '2005-05-24', 'A'),
+	('Misael', 'Shelby', 'Shelby', null, 'A'),
+	('João', 'Pedro', 'Jota', null, 'A'),
+	('Matheus', 'Coelho', 'Mate', null, 'A')
+
+-- Serie B
+	
+INSERT INTO nba.jogadores VALUES 
+	('Haniel', 'Souza', 'Hani', NULL, 'B'),
+	('Filipe', 'Mendes', 'Lipe', '2009-03-03', 'B'),
+	('Gabriel', null, 'Mouse', null, 'B'),
+	('Guilherme', 'Rocha', 'Gui', null, 'B'),
+	('Guilherme', null, null, null, 'B'),
+	('Lucas', null, null, null, 'B')
+  
+  
+  
+  
+-- Inserir um jogador: nome, sobrenomme, apelido, datanascimentoYYYY-MM-DD, serie
+EXEC nba.InserirJogador 'Nome', 'Sobrenome', 'Apelido', '1990-01-01', 'A';
+
+-- Inserir um local: nome, cidade
+EXEC nba.InserirLocal 'Academia Leao de Juda', 'Aguas Lindas';
+
+-- Inserir uma partida: data, localID, Jogador1, Vitorias1, Derrotas1, Jogador2, Vitorias2, Derrotas2
+EXEC nba.InserirPartida '2023-05-15', 1, 9, 3, 2, 10, 2, 3;
+EXEC nba.InserirPartida '2023-05-15', 1, 7, 3, 2, 11, 2, 3;
+
+EXEC nba.InserirPartida '2023-05-16', 1, 8, 3, 2, 12, 2, 3;
+select * from nba.partidas
+-- Inserir estatísticas do jogo: partidaID, JogadorID, Cestasde2, Cestasde3, Faltas, CestasLivres
+EXEC nba.InserirEstatisticasJogo 1, 9, 16, 12, 12, 2;
+EXEC nba.InserirEstatisticasJogo 1, 10, 5, 19, 17, 0;
+
+EXEC nba.InserirEstatisticasJogo 2, 7, 16, 5, 20, 0;
+EXEC nba.InserirEstatisticasJogo 2, 11, 18, 14, 4, 1;
+
+EXEC nba.InserirEstatisticasJogo 3, 8, 46, 99, 7, NULL;
+EXEC nba.InserirEstatisticasJogo 3, 12, 14, 34, 2, null;
